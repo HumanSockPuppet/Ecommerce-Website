23f74cadb73627c2f90fd37c718b5dbd3e021b3a
Brix Aaron Monton <montonbrix123@gmail.com>
2024-04-22 16:04:38 +0800

    feat/jira-001-navbar
    rearranged CSS properties alphabetically

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import AvatarImage from "../assets/image-avatar.png"
 
 const S = {
     NavbarContainer : styled.nav`
+        align-items: center;
         background: none;
         display: flex;
         flex-direction: column;
@@ -14,120 +15,120 @@ const S = {
         position: fixed;
         top: 0;
         width: 100vw;
-        align-items: center;
     `,
     LeftContainer : styled.div`
-        flex: 70%;
-        display: flex;
         align-items: center;
+        display: flex;
+        flex: 70%;
         padding-left: 0%;
-    `,    
-    NavbarInnerContainer : styled.div`    
+    `,
+    NavbarInnerContainer : styled.div`
         background-color: orange;
         display: flex;
         height: 80px;
         position: fixed;
         top: 0;
         width: 100vw;
-    `, 
+    `,
     NavbarLogo : styled.img`
         height: 20px;
-        width: auto;
         margin: 10px;
         margin-left: 20px;
+        width: auto;
     `,
     NavbarLinkContainer: styled.div`
         display: flex;
     `,
     NavbarLink: styled(Link)`
         color: white;
-        font-size: 25px;
         font-family: Arial, Helvetica, sans-serif;
-        text-decoration: none;
+        font-size: 25px;
         margin: 10px;
+        text-decoration: none;
 
-        @media (max-width: 760px){
+        @media (max-width: 760px) {
             display: none;
         }
-    `,    
+    `,
     RightContainer : styled.div`
-        flex: 30%;
-        display: flex;
         align-items: center;
+        display: flex;
+        flex: 30%;
         justify-content: flex-end;
         padding-right: 5%;
     `,
     CartButton : styled.button`
-        background: none;
-        cursor: pointer;
-        border: none;
         align-items: center;
+        background: none;
+        border: none;
+        cursor: pointer;
+        display: flex;
         justify-content: center;
-        outline: none !important; 
         margin: 0px;
+        outline: none !important;
         padding: 0px;
     `,
-    CartLogo : styled.img`        
+    CartLogo : styled.img`
         height: 30px;
-        width: auto;
         margin: 10px;
+        width: auto;
     `,
     CartItems : styled.div`
+        align-items: center;
+        background-color: orange;
         display: flex;
         height: 50vh;
-        width: 90vw;
-        background-color: orange;
-        margin-top: 100px;
-        align-items: center;
         justify-content: center;
+        margin-top: 100px;
+        width: 90vw;
     `,
     Avatar : styled.img`
         height: 40px;
-        width: auto;
         margin: 0px;
+        width: auto;
     `,
     OpenLinkButton : styled.button`
-        font-size: 30px;
-        background: none;
-        cursor: pointer;
-        color: white;
-        border: none;
         align-items: center;
-        justify-content: center;
+        background: none;
+        border: none;
+        color: white;
+        cursor: pointer;
         display: inline-flex;
-        outline: none !important; 
+        font-size: 30px;
+        justify-content: center;
         margin: 0px;
+        outline: none !important;
         padding: 15px;
 
-        @media (min-width: 761px){
+        @media (min-width: 761px) {
             display: none;
         }
-    `,    
+    `,
     NavbarExtendedContainer : styled.div`
+        align-items: center;
         display: flex;
         flex-direction: column;
-        align-items: center;
         height: ${(props) => (props.extendNavbar ? "325px" : "80px")};
         width: 100vw;
 
-        @media (min-width: 761px){
+        @media (min-width: 761px) {
             display: none;
         }
     `,
     NavbarExtendedLinkContainer: styled.div`
-        margin-top: 80px;
-        display: flex;
-        flex-direction: column;  
         align-items: center;
-        background: orange;    
-        width: 100%;             
+        background: orange;
+        display: flex;
+        flex-direction: column;
+        margin-top: 80px;
+        width: 100%;
     `,
     NavbarLinkExtended: styled(Link)`
         color: white;
-        font-size: 25px;
         font-family: Arial, Helvetica, sans-serif;
-        text-decoration: none;
+        font-size: 25px;
         margin: 10px;
+        text-decoration: none;
     `
 }
 
